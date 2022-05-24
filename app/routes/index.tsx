097952ff3b011c79fb30645fdd5a43ea7477f218
Ratnam Yadav <ratnam.yadav@icloud.com>
2022-05-24 16:04:00 +0530

    styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,32 @@
-export default function IndexRoute() {
-    return <div>Hello Index Route</div>;
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: stylesUrl,
+        },
+    ];
+};
+
+export default function Index() {
+    return (
+        <div className="container">
+            <div className="content">
+                <h1>
+                    Remix <span>Jokes!</span>
+                </h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="jokes">Read Jokes</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 }
